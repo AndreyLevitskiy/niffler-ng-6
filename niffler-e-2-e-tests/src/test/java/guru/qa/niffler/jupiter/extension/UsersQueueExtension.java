8c55cf393131462353cc5baabc2c010fc5c72dbc
Andrey Levitskiy <a.levitskiy@space307.com>
2024-12-18 11:41:40 +0200

    3.2 third and half of fourth points

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
@@ -19,22 +19,33 @@ public class UsersQueueExtension implements BeforeEachCallback, AfterEachCallbac
 
     public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(UsersQueueExtension.class);
 
-    public record StaticUser(String name, String password, boolean empty) {
+    public record StaticUser(String username,
+                             String password,
+                             String friend,
+                             String income,
+                             String outcome) {
     }
 
     private final static Queue<StaticUser> EMPTY_USERS = new ConcurrentLinkedQueue<>();
-    private final static Queue<StaticUser> NOT_EMPTY_USERS = new ConcurrentLinkedQueue<>();
+    private static final Queue<StaticUser> WITH_FRIEND_USERS = new ConcurrentLinkedQueue<>();
+    private static final Queue<StaticUser> WITH_INCOME_REQUEST_USERS = new ConcurrentLinkedQueue<>();
+    private static final Queue<StaticUser> WITH_OUTCOME_REQUEST_USERS = new ConcurrentLinkedQueue<>();
 
     static {
-        EMPTY_USERS.add(new StaticUser("gale.bernier", "12345", true));
-        NOT_EMPTY_USERS.add(new StaticUser("duck", "12345", false));
-        NOT_EMPTY_USERS.add(new StaticUser("user", "password", false));
+        EMPTY_USERS.add(new StaticUser("gale.bernier", "12345", null, null, null));
+        WITH_FRIEND_USERS.add(new StaticUser("duck", "12345", "gale.bernier", null, null));
+        WITH_INCOME_REQUEST_USERS.add(new StaticUser("user", "password", null, "john", null));
+        WITH_OUTCOME_REQUEST_USERS.add(new StaticUser("admin", "12345", null, null, "kyle"));
     }
 
     @Target(ElementType.PARAMETER)
     @Retention(RetentionPolicy.RUNTIME)
     public @interface UserType {
-        boolean empty() default true;
+        Type value() default Type.EMPTY;
+
+        enum Type {
+            EMPTY, WITH_FRIEND, WITH_INCOME_REQUEST, WITH_OUTCOME_REQUEST
+        }
     }
 
     @Override
@@ -44,19 +55,19 @@ public class UsersQueueExtension implements BeforeEachCallback, AfterEachCallbac
                 .forEach(
                         p -> {
                             UserType ut = p.getAnnotation(UserType.class);
-
                             Optional<StaticUser> user = Optional.empty();
                             StopWatch sw = StopWatch.createStarted();
+
                             while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
-                                user = ut.empty()
-                                        ? Optional.ofNullable(EMPTY_USERS.poll())
-                                        : Optional.ofNullable(NOT_EMPTY_USERS.poll());
+                                user = Optional.ofNullable(getUserFromQueue(ut).poll());
                             }
+
                             Allure.getLifecycle().updateTestCase(
                                     tc -> {
                                         tc.setStart(new Date().getTime());
                                     }
                             );
+
                             user.ifPresentOrElse(
                                     u -> {
                                         ((Map<UserType, StaticUser>) context.getStore(NAMESPACE)
@@ -78,13 +89,20 @@ public class UsersQueueExtension implements BeforeEachCallback, AfterEachCallbac
         Map<UserType, StaticUser> map = context.getStore(NAMESPACE).get(
                 context.getUniqueId(),
                 Map.class);
+
         for (Map.Entry<UserType, StaticUser> e : map.entrySet()) {
-            if (e.getKey().empty()) {
-                EMPTY_USERS.add(e.getValue());
-            } else {
-                NOT_EMPTY_USERS.add(e.getValue());
-            }
+            UserType type = e.getKey();
+            getUserFromQueue(type).add(e.getValue());
         }
+    }
+
+    private static Queue<StaticUser> getUserFromQueue(UserType type) {
+        return switch (type.value()) {
+            case UserType.Type.EMPTY -> EMPTY_USERS;
+            case UserType.Type.WITH_FRIEND -> WITH_FRIEND_USERS;
+            case UserType.Type.WITH_INCOME_REQUEST -> WITH_INCOME_REQUEST_USERS;
+            case UserType.Type.WITH_OUTCOME_REQUEST -> WITH_OUTCOME_REQUEST_USERS;
+        };
     }
 
     @Override
