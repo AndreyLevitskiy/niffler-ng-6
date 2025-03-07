87832133720a9b018a0a14a8ecd53d1caf071d36
Andrey Levitskiy <a.levitskiy@space307.com>
2025-03-07 19:48:12 +0200

    5.1 full stream

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/AuthUserDaoSpringJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/AuthUserDaoSpringJdbc.java
@@ -4,8 +4,12 @@ import guru.qa.niffler.data.dao.AuthUserDao;
 import guru.qa.niffler.data.entity.auth.AuthUserEntity;
 import guru.qa.niffler.data.mapper.AuthUserEntityRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,7 +23,25 @@ public class AuthUserDaoSpringJdbc implements AuthUserDao {
 
     @Override
     public AuthUserEntity create(AuthUserEntity user) {
-        return null;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        KeyHolder kh = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO \"user\" (username, password, enabled, account_non_expired, account_non_locked, credentials_non_expired) VALUES (?, ?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.setBoolean(3, user.getEnabled());
+            ps.setBoolean(4, user.getAccountNonExpired());
+            ps.setBoolean(5, user.getAccountNonLocked());
+            ps.setBoolean(6, user.getAccountNonExpired());
+            return ps;
+        }, kh);
+
+        final UUID generatedKey = (UUID) kh.getKeys().get("id");
+        user.setId(generatedKey);
+        return user;
     }
 
     @Override
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UdUserDaoSpringJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UdUserDaoSpringJdbc.java
@@ -1,4 +1,59 @@
 package guru.qa.niffler.data.dao.impl;
 
-public class UdUserDaoSpringJdbc {
+import guru.qa.niffler.data.dao.UdUserDao;
+import guru.qa.niffler.data.entity.userdata.UserEntity;
+import guru.qa.niffler.data.mapper.UdUserEntityRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UdUserDaoSpringJdbc implements UdUserDao {
+
+    private final DataSource dataSource;
+
+    public UdUserDaoSpringJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public UserEntity create(UserEntity user) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        KeyHolder kh = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO \"user\" (username, currency, firstname, surname full_name, photo, photo_small) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getCurrency().name());
+            ps.setString(3, user.getFirstname());
+            ps.setString(4, user.getSurname());
+            ps.setString(5, user.getFullname());
+            ps.setBytes(6, user.getPhoto());
+            ps.setBytes(7, user.getPhotoSmall());
+            return ps;
+        }, kh);
+
+        final UUID generatedKey = (UUID) kh.getKeys().get("id");
+        user.setId(generatedKey);
+        return user;
+    }
+
+    @Override
+    public Optional<UserEntity> findById(UUID id) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                        "SELECT * FROM \"user\" WHERE id = ?",
+                        UdUserEntityRowMapper.instance,
+                        id
+                )
+        );
+    }
 }
